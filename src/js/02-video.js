269b0f6d8dea8fe93f8974c8ac943c9167dc255b
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-11-22 01:31:42 +0200

    tast 2 package install

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+// import SimpleLightbox from "simplelightbox";
+import Player from '@vimeo/player';
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
