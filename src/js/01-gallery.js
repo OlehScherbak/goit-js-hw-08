63255acf199cda0e560a0d36c02dad093b42a9ea
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-11-22 00:57:50 +0200

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captions: true, captionPosition: 'bottom', captionsData: 'alt', captionDelay: 250 });
