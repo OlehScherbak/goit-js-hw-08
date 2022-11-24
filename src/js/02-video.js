3b0b692e72232b849a5e46feb318e920fbf15142
Oleh.Scherbak <oleh.scherbak@ukr.net>
2022-11-24 22:11:00 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,13 @@
 import Player from '@vimeo/player';
-// import _ from 'lodash.throttle';
-// var _ = require('lodash.throttle');
+import throttle from 'lodash.throttle';
+
 const player = new Player('vimeo-player');
 
 let currentTime = localStorage.getItem('videoplayer-current-time');
 currentTime = currentTime ? currentTime : 0;
 player.setCurrentTime(currentTime);
 
-player.on('timeupdate', _.throttle(currentTimeUpdate, 1000));
+player.on('timeupdate', throttle(currentTimeUpdate, 1000));
 
 function currentTimeUpdate(evt) {
   localStorage.setItem('videoplayer-current-time', evt.seconds);
